9991e94eb4731a448dec70b532eeadabb4e9dcfc
Ahmed Amjad <ahmed_aaa1988@yahoo.com>
2024-07-27 01:57:24 +0500

    Update tables.php

--- Database/tables.php
+++ Database/tables.php
@@ -1,7 +1,7 @@
 <?php
 
 $path = $_SERVER['DOCUMENT_ROOT'];
-require_once $path . "/attendanceapp/database/database.php";
+require_once $path . "/Attendance-App/Database/database.php";
 function clearTable($dbo, $tabName)
 {
   $c = "delete from ".$tabName;
