9f695d90509c989a1e005e96409f38fc92efbf31
Ahmed Amjad <ahmed_aaa1988@yahoo.com>
2024-07-27 00:04:57 +0500

    Update database.php

--- Database/database.php
+++ Database/database.php
@@ -5,7 +5,7 @@ $password = "";
 $dbname = "attendance_app";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
